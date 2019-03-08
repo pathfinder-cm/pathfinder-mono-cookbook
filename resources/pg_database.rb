a5174876727565fc826038afebcf6051dab2a20b
Tara Baskara <tara.baskara@gmail.com>
2019-03-08 10:57:59 +0700

    [tara] Declare default locale value

--- resources/pg_database.rb
+++ resources/pg_database.rb
@@ -1,7 +1,7 @@
 property :database,  String, name_property: true
 property :user,      String, default: 'postgres', required: true
 property :encoding,  String, default: 'UTF-8'
-property :locale,    String
+property :locale,    String, default: 'C.UTF-8'
 
 action :create do
   createdb = 'createdb'
